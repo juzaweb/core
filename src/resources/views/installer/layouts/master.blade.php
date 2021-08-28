c8e288997bf3ec8f86543dedf6a02c2d06e81ece
Juzaweb <dangtheanh16@gmail.com>
2021-08-28 12:26:11 +0700

    :bug: Fix active install

--- src/resources/views/installer/layouts/master.blade.php
+++ src/resources/views/installer/layouts/master.blade.php
@@ -55,7 +55,7 @@
                         @endif
                     </li>
                     <li class="step__divider"></li>
-                    <li class="step__item {{ is_active_route('juzaweb::installer.requirements') }}">
+                    <li class="step__item {{ is_active_route('installer.requirements') }}">
                         @if(Request::is('install') || Request::is('install/requirements') || Request::is('install/permissions') || Request::is('install/environment') || Request::is('install/environment/wizard') || Request::is('install/environment/classic') )
                             <a href="{{ route('installer.requirements') }}">
                                 <i class="step__icon fa fa-list" aria-hidden="true"></i>
@@ -65,7 +65,7 @@
                         @endif
                     </li>
                     <li class="step__divider"></li>
-                    <li class="step__item {{ is_active_route('juzaweb::installer.welcome') }}">
+                    <li class="step__item {{ is_active_route('installer.welcome') }}">
                         @if(Request::is('install') || Request::is('install/requirements') || Request::is('install/permissions') || Request::is('install/environment') || Request::is('install/environment/wizard') || Request::is('install/environment/classic') )
                             <a href="{{ route('installer.welcome') }}">
                                 <i class="step__icon fa fa-home" aria-hidden="true"></i>
