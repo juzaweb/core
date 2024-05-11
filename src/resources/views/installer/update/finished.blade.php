e8259439bbd76b802a921f9d2cdc22597dd8bf0d
Juzaweb <47020363+juzaweb@users.noreply.github.com>
2024-05-11 14:15:58 +0000

    :tada: CMS Core

--- src/resources/views/installer/update/finished.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('juzaweb::installer.layouts.master-update')
-
-@section('title', trans('juzaweb::installer.updater.final.title'))
-@section('container')
-    <p class="paragraph text-center">{{ session('message')['message'] }}</p>
-    <div class="buttons">
-        <a href="{{ url('/') }}" class="button">{{ trans('juzaweb::installer.updater.final.exit') }}</a>
-    </div>
-@stop
