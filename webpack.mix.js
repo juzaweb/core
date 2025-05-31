7c6d9d17a5ab5771b85c6001a306c216a5d34358
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-05-31 20:50:13 +0700

    :+1: Add more plugins

--- webpack.mix.js
+++ webpack.mix.js
@@ -25,6 +25,9 @@ mix.combine(
         `assets/plugins/jquery/jquery.min.js`,
         `assets/plugins/bootstrap/js/bootstrap.bundle.min.js`,
         `assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js`,
+        `assets/plugins/select2/js/select2.full.min.js`,
+        `assets/plugins/jquery-validation/jquery.validate.min.js`,
+        `assets/plugins/jquery-validation/additional-methods.min.js`,
         `assets/dist/js/admin.min.js`,
     ],
     `assets/public/js/admin.min.js`
@@ -34,7 +37,10 @@ mix.styles(
     [
         `assets/plugins/fontawesome/css/all.min.css`,
         `assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css`,
+        `assets/plugins/select2/css/select2.min.css`,
+        `assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css`,
         `assets/dist/css/admin.min.css`,
+        `assets/dist/css/custom.css`,
     ],
     `assets/public/css/admin.min.css`
 ).mergeManifest();
