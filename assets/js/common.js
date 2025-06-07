22f117fa5bc558d9bc875fff041beb18cfb62ae0
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-06-07 10:42:48 +0700

    :+1: Update assets

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,13 +1,13 @@
-$(function () {
-    function initSelect2(parent = 'body') {
-        $(parent + ' .select2').each(function () {
-            $(this).select2({
-                theme: 'bootstrap4',
-                width: $(this).data('width') || '100%',
-                dropdownAutoWidth: !$(this).data('width'),
-            });
+function initSelect2(parent = 'body') {
+    $(parent + ' .select2').each(function () {
+        $(this).select2({
+            theme: 'bootstrap4',
+            width: $(this).data('width') || '100%',
+            dropdownAutoWidth: !$(this).data('width'),
         });
-    }
+    });
+}
 
+$(function () {
     initSelect2('body');
 });
