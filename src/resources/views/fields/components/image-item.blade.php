949bda4d9bf4aba83dea3de4e04062665c4e4442
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-06-07 12:19:34 +0700

    Escapes URL in image item and improves editor layout
    Escapes the URL in the image item component to ensure proper rendering.
    
    Improves the layout of the editor field for better readability.

--- src/resources/views/fields/components/image-item.blade.php
+++ src/resources/views/fields/components/image-item.blade.php
@@ -2,6 +2,6 @@
     <a href="javascript:void(0)" class="text-danger remove-image-item">
         <i class="fa fa-trash"></i>
     </a>
-    <div class="image-preview" style="background-image: url({{ $url }});"></div>
+    <div class="image-preview" style="background-image: url('{{ $url }}');"></div>
     <input type="hidden" name="{{ $name }}" class="input-path" value="{{ $path }}">
 </div>
--- src/resources/views/fields/editor.blade.php
+++ src/resources/views/fields/editor.blade.php
@@ -1,9 +1,10 @@
 <div class="form-group">
     <label for="{{ $options['id'] ?? $name }}">{{ $label }}</label>
-    <textarea class="form-control jw-editor {{ isset($options['classes']) ? implode(' ', $options['classes']) : '' }}"
-              name="{{ $name }}"
-              id="{{ $options['id'] ?? $name }}"
-              rows="{{ Arr::get($options, 'rows', 5) }}"
+    <textarea
+            class="form-control jw-editor {{ isset($options['classes']) ? implode(' ', $options['classes']) : '' }}"
+            name="{{ $name }}"
+            id="{{ $options['id'] ?? $name }}"
+            rows="{{ Arr::get($options, 'rows', 5) }}"
             @foreach(Arr::except($options, ['classes', 'id', 'rows']) as $key => $value)
                 {{ $key }}="{{ $value }}"
             @endforeach
