8611339ed4e7242eb84a46ba3ddd3c7557c679a6
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-06-07 14:57:43 +0700

    feat: improve setting fields (fields-support)
    Refactor setting fields to improve usability and
    clarity.  Added site name field, rearranged
    layout, and updated field labels for better
    understanding.  Replaced logo and icon fields
    with image fields.  Improved language selection
    with dynamic dropdown list.

--- src/resources/views/admin/setting/index.blade.php
+++ src/resources/views/admin/setting/index.blade.php
@@ -7,20 +7,25 @@
         </div>
         <div class="card-body">
             <div class="row">
-                <div class="col-md-8">
-                    {{ Field::text(__('Site Title'), 'site_title', ['placeholder' => __('Site Title')]) }}
+                <div class="col-md-9">
+                    {{ Field::text(__('Site Title'), 'title', ['placeholder' => __('Site Title')]) }}
 
                     {{ Field::textarea(__('Site Description'), 'description', ['placeholder' => __('Site Description')]) }}
 
-                    {{ Field::select(__('Language'), 'language')->dropDownList(['en' => 'Eng']) }}
+                    {{ Field::text(__('Site Name'), 'sitename', ['placeholder' => __('Ex: Juzaweb')]) }}
 
-                    {{ Field::checkbox(__('Enable User Registration'), 'user_registration', ['value' => setting('user_registration')]) }}
+                    {{ Field::select(__('Language'), 'language', ['value' => config('language', 'en')])
+                        ->dropDownList(collect(config('locales'))->pluck('name', 'code')->toArray()) }}
 
-                    {{ Field::checkbox(__('Enable User Verification'), 'user_verification', ['value' => setting('user_verification')]) }}
+                    {{ Field::checkbox(__('User Registration'), 'user_registration', ['value' => setting('user_registration')]) }}
+
+                    {{ Field::checkbox(__('User Verification'), 'user_verification', ['value' => setting('user_verification')]) }}
                 </div>
 
-                <div class="col-md-4">
+                <div class="col-md-3">
+                    {{ Field::image(__('Logo'), 'logo', ['value' => setting('icon')]) }}
 
+                    {{ Field::image(__('Icon'), 'icon', ['value' => setting('icon')]) }}
                 </div>
             </div>
         </div>
