d54ecd45cd8440d73cfb9295e9ee9e0a2c61b8e3
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-06-14 10:45:15 +0700

    feat: move user routes to app module (#1)
    Move user routes from core module to app module.  This
    improves modularity and maintainability of the
    application.

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -7,15 +7,8 @@
  * @link       https://cms.juzaweb.com
  */
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use Juzaweb\Core\Facades\RouteResource;
 use Juzaweb\Core\Http\Controllers\Admin\SettingController;
-
-Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
-
-RouteResource::admin('users', UserController::class);
 
 // RouteResource::admin('roles', UserController::class);
 
