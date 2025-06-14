f549a8df6fc01feafcacebeec36e911b35178354
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-06-14 20:51:25 +0700

    feat: Add ThemeController (master)
    Add ThemeController file to handle theme
    related functionalities.

---
+++ src/Http/Controllers/ThemeController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * JUZAWEB CMS - Laravel CMS for Your Project
+ *
+ * @package    juzaweb/cms
+ * @author     The Anh Dang
+ * @link       https://cms.juzaweb.com
+ * @license    GNU V2
+ */
+
+namespace Juzaweb\Core\Http\Controllers;
+
+use Juzaweb\Core\Traits\HasSessionResponses;
+
+class ThemeController extends Controller
+{
+    use HasSessionResponses;
+}
