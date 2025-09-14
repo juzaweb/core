20b152d4b4534b59cb22b6ba078e768545628753
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-09-14 16:41:46 +0700

    Adjusts column widths in bulk actions and search components for improved layout

--- src/resources/views/components/datatables/bulk-actions.blade.php
+++ src/resources/views/components/datatables/bulk-actions.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-sm-12 col-md-9">
+    <div class="col-sm-12 col-md-8">
         @if(count($bulkActions) > 0)
             <div class="btn-group float-left jw-datatable-bulk-actions" role="group" aria-label="User Actions">
                 <button type="button"
@@ -31,7 +31,7 @@
     </div>
 
     @if($searchable)
-    <div class="col-sm-12 col-md-3">
+    <div class="col-sm-12 col-md-4">
         <div id="jw-datatable_filter" class="jw-datatable_filter">
             <label>{{ __('Search') }}: <input type="search" class="form-control form-control-sm" placeholder="" aria-controls="jw-datatable"></label>
         </div>
