c92e58b8e0a773c5a75213075304c44bc8603d44
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-01-20 11:13:43 +0700

    feat(datatables): Conditionally hide the 'translate' bulk action based on the translator configuration.

--- src/resources/views/components/datatables/bulk-actions.blade.php
+++ src/resources/views/components/datatables/bulk-actions.blade.php
@@ -1,25 +1,20 @@
 <div class="row">
     <div class="col-sm-12 col-md-8">
-        @if(count($bulkActions) > 0)
+        @if (count($bulkActions) > 0)
             <div class="btn-group float-left jw-datatable-bulk-actions" role="group" aria-label="User Actions">
-                <button type="button"
-                        class="btn btn-secondary dropdown-toggle"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                        disabled
-                >
+                <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false" disabled>
                     {{ __('core::translation.bulk_actions') }}
                 </button>
                 <div class="dropdown-menu">
-                    @foreach($bulkActions as $action)
-                        <a
-                                class="dropdown-item jw-datatable-bulk-action jw-datatable-bulk-action_{{ $action->getAction() }} {{ $action->getColor() ? 'text-' . $action->getColor() : '' }}"
-                                href="javascript:void(0)"
-                                data-action="{{ $action->getAction() }}"
-                                data-type="{{ $action->getType() }}"
-                                data-endpoint="{{ $endpoint }}"
-                        >
+                    @foreach ($bulkActions as $action)
+                        @if ($action->getAction() === 'translate' && !config('translator.enable'))
+                            @continue
+                        @endif
+
+                        <a class="dropdown-item jw-datatable-bulk-action jw-datatable-bulk-action_{{ $action->getAction() }} {{ $action->getColor() ? 'text-' . $action->getColor() : '' }}"
+                            href="javascript:void(0)" data-action="{{ $action->getAction() }}"
+                            data-type="{{ $action->getType() }}" data-endpoint="{{ $endpoint }}">
                             <i class="{{ $action->getIcon() }}"></i>
 
                             {{ $action->getLabel() }}
@@ -30,12 +25,13 @@
         @endif
     </div>
 
-    @if($searchable)
-    <div class="col-sm-12 col-md-4">
-        <div id="jw-datatable_filter" class="jw-datatable_filter">
-            <label>{{ __('core::translation.search') }}: <input type="search" class="form-control form-control-sm" placeholder="" aria-controls="jw-datatable"></label>
+    @if ($searchable)
+        <div class="col-sm-12 col-md-4">
+            <div id="jw-datatable_filter" class="jw-datatable_filter">
+                <label>{{ __('core::translation.search') }}: <input type="search" class="form-control form-control-sm"
+                        placeholder="" aria-controls="jw-datatable"></label>
+            </div>
         </div>
-    </div>
     @endif
 
 </div>
