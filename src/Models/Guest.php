748ab0fda89807102a7036abc2b4f72c5b3f9d5f
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-01-20 13:46:49 +0700

    feat: add Guest model with fillable attributes and UUID support

---
+++ src/Models/Guest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * JUZAWEB CMS - Laravel CMS for Your Project
+ *
+ * @package    juzaweb/cms
+ * @author     The Anh Dang
+ * @link       https://cms.juzaweb.com
+ */
+
+namespace Juzaweb\Modules\Core\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Notifications\Notifiable;
+use Juzaweb\Modules\Core\Models\Model;
+
+class Guest extends Model
+{
+    use HasUuids, Notifiable;
+
+    protected $table = 'guests';
+
+    protected $fillable = [
+        'ipv4',
+        'ipv6',
+        'user_agent',
+        'name',
+        'email',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+    ];
+}
