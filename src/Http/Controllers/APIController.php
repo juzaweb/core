e3289dfaa1c961442cf85f3124724c8ff7f5e917
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-08 11:25:20 +0700

    ✨ feat(APIController): Add method to retrieve and validate request limit from request

--- src/Http/Controllers/APIController.php
+++ src/Http/Controllers/APIController.php
@@ -14,4 +14,15 @@ use Juzaweb\Modules\Core\Traits\HasRestResponses;
 abstract class APIController extends Controller
 {
     use HasRestResponses;
+
+    protected function getLimitRequest()
+    {
+        $limit = request()->get('limit', 10);
+
+        if (!is_numeric($limit) || $limit <= 0 || $limit > 100) {
+            $limit = 10;
+        }
+
+        return (int) $limit;
+    }
 }
